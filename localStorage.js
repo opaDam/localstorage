e9c1e421ccf2b5f3d6aae6680272324e4fc65e99
opaDam <54212616+opaDam@users.noreply.github.com>
2022-01-23 15:40:26 +0100

    save

--- localStorage.js
+++ localStorage.js
@@ -1,23 +1,23 @@
-// localStorage /////////////////////////////////////////////////
+// ?localStorage /////////////////////////////////////////////////
 const addData = document.querySelector("#add");
 const delData = document.querySelector("#del");
 const prt_data = document.querySelector("#prt_data");
 
 let existingEntries = [];
-let del_item = "data10";
-let add_item = "data2";
+// let del_item = "";
+// let add_item = "";
 // ****************************************************************
 // read Localstorage and Print data /////////////////////////////
- existingEntries = JSON.parse(
-    localStorage.getItem("list_items") || "[]"
-  );
-  prt_data.innerText = existingEntries;
+existingEntries = JSON.parse(
+  localStorage.getItem("list_items") || "[]"
+);
+prt_localstorage.innerText = existingEntries; //? PRINT
 // ****************************************************************
 function addItemLocaLstorage(add_item) {
   // parse existing storage key or string representation of empty array
-//    existingEntries = JSON.parse(
-//     localStorage.getItem("list_items") || "[]"
-//   );
+  existingEntries = JSON.parse(
+    localStorage.getItem("list_items") || "[]"
+  );
   // Add item if it's not already in the array, then store array again
   if (!existingEntries.includes(add_item)) {
     existingEntries.push(add_item);
@@ -27,13 +27,13 @@ function addItemLocaLstorage(add_item) {
     // or tell user it's already there
     console.log(add_item + " already exists");
   }
-  prt_data.innerText = existingEntries;
+  prt_localstorage.innerText = existingEntries; //? PRINT
 }
 // ****************************************************************
 function removeItemLocalstorage(del_item) {
-//  existingEntries = JSON.parse(
-//     localStorage.getItem("list_items") || "[]"
-//   );
+  existingEntries = JSON.parse(
+    localStorage.getItem("list_items") || "[]"
+  );
   for (var i = 0; i < existingEntries.length; i++) {
     if (existingEntries[i] === del_item) {
       existingEntries.splice(i, 1);
@@ -42,13 +42,14 @@ function removeItemLocalstorage(del_item) {
   }
   localStorage.setItem("list_items", JSON.stringify(existingEntries));
   console.log(existingEntries);
-  prt_data.innerText = existingEntries;
+  prt_localstorage.innerText = existingEntries; // ?PRINT
 }
 // *****************************************************************
-  function delAllItemLocalstorage(){
-// localStorage.removeItem(key);  //item
-localStorage.clear(); //all items
-existingEntries = [];
-prt_data.innerText = existingEntries;
-  }
+function delAllItemLocalstorage() {
+  key = "list_items"
+  localStorage.removeItem(key);  //item
+  localStorage.clear(); //all items
+  existingEntries = [];
+  prt_localstorage.innerText = existingEntries; //? PRINT
+}
   // einde localStorage ///////////////////////////////////////////
